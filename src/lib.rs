2c468d41eef11e7c0556b7b262086f2966274a1e
Thom Wiggers <thom@thomwiggers.nl>
2018-05-15 13:41:22 +0200

    System allocator feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+#![cfg_attr(feature="system_alloc", feature(alloc_system))]
 //! Fast binary matrix operations
 //! Interfaces the M4RI library and provides friendly abstractions
 extern crate libc;
 #[cfg_attr(test, macro_use)]
 extern crate vob;
+
+#[cfg(feature="system_alloc")]
+extern crate alloc_system;
 
 extern crate rand;
 
