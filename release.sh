ad218fd4ea643d7fc86cc8ea9014d7bc3b950673
Thom Wiggers <thom@thomwiggers.nl>
2018-11-19 13:17:24 +0100

    Add release script

---
+++ release.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = "" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+version="$1"
+version_sane="$(echo $version | sed -Ee 's/^([0-9]{1,}\.){2}[0-9]{1,}$/YES/')"
+if [ "$version_sane" != "YES" ]; then
+    echo "Version should be in the format '12.34.56'"
+    exit 1
+fi
+
+if [[ $(git diff --stat) != '' ]]; then
+    echo "Dirty working directory"
+    exit 1
+fi
+
+echo "Making sure we're up-to-date"
+git fetch --tags
+
+tag="$(git tag --contains)"
+branch="$(git symbolic-ref --quiet --short HEAD || git rev-parse --short HEAD)"
+if [ "$tag" = "v$version" ]; then
+    echo "Already tagged!"
+    exit 1
+fi
+
+echo "Updating Cargo.toml version"
+sed -i "s/version = \"[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\"  # Package version/version = \"$version\"  # Package version/" Cargo.toml
+if grep -q "version = \"$version\"" Cargo.toml; then
+    echo "Version update in Cargo.toml succeeded"
+else
+    echo "Version update failed"
+fi
+
+echo "Committing Cargo.toml"
+git add Cargo.toml
+git commit -m "Releasing version $version"
+
+echo "Creating tag"
+git tag -s "v$version"
+
+echo "Building cargo release"
+cargo publish --no-verify
+
+echo "Pushing git objects"
+git push
+git push --tags
